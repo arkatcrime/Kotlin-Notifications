842f15667685bf85e7d365fe541dc41930d3c8d3
João Oliveira <joljoa3@jci.com>
2020-04-14 11:22:53 +0100

    1.25 Exercise: Foreground vs. Background

--- app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -1,12 +1,18 @@
 package com.example.android.eggtimernotifications
 
+import android.app.NotificationManager
+import android.content.Context
 import android.util.Log
+import androidx.core.content.ContextCompat
+import com.example.android.eggtimernotifications.util.sendNotification
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
-    private val TAG = "MyFbMessagingService"
+    companion object {
+        private const val TAG = "MyFirebaseMsgService"
+    }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
         Log.d(TAG, "From: ${remoteMessage?.from}" )
@@ -15,10 +21,22 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         remoteMessage?.data?.let {
             Log.d(TAG, "Message data payload: " + remoteMessage.data)
         }
+
+        // TODO: Step 3.6 check messages for notification and call sendNotification
+        // Check if message contains a notification payload.
+        remoteMessage?.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+            sendNotification(it.body!!)
+        }
     }
 
     private fun sendRegistrationToServer(token: String?){
+        //TODO: Implement this method to send token to your app server
+    }
 
+    private fun sendNotification(messageBody: String){
+        val notificationManager = ContextCompat.getSystemService(applicationContext, NotificationManager::class.java) as NotificationManager
+        notificationManager.sendNotification(messageBody, applicationContext)
     }
 
     override fun onNewToken(token: String?) {
